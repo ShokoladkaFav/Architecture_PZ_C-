fe6ee2b7d768f38ae39959e3cac79b401c30f227
ShokoladkaP <stolikk756@gmail.com>
2025-11-21 20:21:13 +0200

    Update project files

---
+++ ECommerce.API/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Users.Commands.CreateUser;
+using Application.Features.Users.Queries.GetUserById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // POST: api/users (Реєстрація)
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand command)
+        {
+            var userId = await _mediator.Send(command);
+            return Ok(userId);
+        }
+
+        // GET: api/users/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var query = new GetUserByIdQuery { Id = id };
+            var userDto = await _mediator.Send(query);
+
+            if (userDto == null)
+            {
+                return NotFound($"User with ID {id} not found.");
+            }
+
+            return Ok(userDto);
+        }
+    }
+}
